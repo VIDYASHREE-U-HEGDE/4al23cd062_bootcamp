671738932a1fd91ac6dec7ca581ab4492a341c01
VIDYASHREE-U-HEGDE <vidyavidyah3@gmail.com>
2026-06-27 16:43:01 +0530

    message

--- Day2/ml_project.py
+++ Day2/ml_project.py
@@ -1,35 +1,44 @@
 import pandas as pd
-import numpy as np 
+import numpy as np
 from sklearn.impute import SimpleImputer
 from sklearn.feature_selection import SelectKBest,mutual_info_regression
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-import os
+import os 
 
 try:
-  
+
     from category_encoders import TargetEncoder
 except ImportError:
     TargetEncoder = None
-    print("warning :category_encoders not installed.target will be unavailable ")
+    print("Warning: category_encoders not found")
 
 
 def main():
-
-    print("loading Datasets")
+    print("loading dataset")
     file_path='train.csv'
+    
 
     if not os.path.exists(file_path):
-        print(f"Error : {file_path} not found.")
+        print(f"Error: {file_path} not found.")
         return
 
-    df=pd.read_csv(file_path)
-    print(f"Datasets Loaded Successfully .Rows:{df.shape[0]},Features:{df.shape[1]}\n")
-    print(df.head())
-    print(df.tail())
+    df = pd.read_csv(file_path)
+    print("Datasets Loaded Successfully.Rows:{df.shape[0]},Features:{df.shape[1]}\n")
 
-    print("\nBasic Statstics:")
-    print(df.describe())
+    print("HANDLING MISSING DATA")
+    print("Artificially deleting some 'Hits' (H) data to demonstrate")
 
-if __name__=="__main__":
+    df.loc[0.25, 'H'] = np.nan
+    imputer = SimpleImputer(strategy='median')
+    df['H'] = imputer.fit_transform(df[['H']])
+    print(f"Imputation completed.'Hits' (h)  now has { df['H'].isnull().sum()} null values.\n")
+
+    print("Evaluating the sentence of the Runs(R) Distribution....")
+
+
+    df["LogRuns"] = np.log1p(df["R"])
+    print(f"Log Transformation Applied. New skewness: {df['LogRuns'].skew():.2f} (closer to 0 is perfectly balanced)\n")
+ 
+if __name__ == "__main__":
     main()
