9167345229f0c8a724ff272a902f913e1f0577f2
VIDYASHREE-U-HEGDE <vidyavidyah3@gmail.com>
2026-06-29 12:00:40 +0530

    message

--- Day2/ml_project.py
+++ Day2/ml_project.py
@@ -1,3 +1,4 @@
+from pandas.core.interchange import dataframe
 import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
@@ -7,7 +8,6 @@ from sklearn.linear_model import LinearRegression
 import os 
 
 try:
-
     from category_encoders import TargetEncoder
 except ImportError:
     TargetEncoder = None
@@ -39,6 +39,45 @@ def main():
 
     df["LogRuns"] = np.log1p(df["R"])
     print(f"Log Transformation Applied. New skewness: {df['LogRuns'].skew():.2f} (closer to 0 is perfectly balanced)\n")
- 
+    print(df.head())
+    df['Team_ID'] = ['Team'+str(np.random.randint(1,150)) for _ in range(len(df))]
+
+    if TargetEncoder is not None:
+        print("Applying Target Encoder")
+
+        encoder = TargetEncoder()
+        print("Missing values in W:", df['W'].isnull().sum())
+        df = df.dropna(subset=['W'])
+        df['Team_ID_Encoded'] = encoder.fit_transform(df['Team_ID'], df['W'])
+
+        print(f"\nTarget Encoding Example (First 5 Rows):")
+        print(df[['Team_ID', 'Team_ID_Encoded', 'W']].head())
+
+    
+    else:
+        print("Category Encoders not installed")
+
+
+
+    features_to_test=['HR','H','ID','BA']
+
+    X_features = df[features_to_test].fillna(0)
+    Y_target = df['W']
+
+    selector = SelectKBest(score_func = mutual_info_regression,k=1)
+    selector.fit(X_features, Y_target)
+        
+    winning_features = selector.get_support()
+    best_feature = X_features.columns[winning_features].tolist()
+    print(f"Best Features:{best_feature}")
+
+    X = df[best_feature]
+    Y= df['W']
+
+    X_train,X_test,y_train,y_test = train_test_split(X,Y,test_size=0.2,random_state=42)
+
+    print(f"trainning Data Size:{X_train.shape}")
+    print(f"Testing Data Size:{X_test.shape}\n")
+    
 if __name__ == "__main__":
     main()
